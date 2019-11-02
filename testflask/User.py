71c3f2cf2fb198629e531b12245bf0f8e299ca24
Michael Hankins <hankinsmic@gmail.com>
2019-11-02 14:29:28 -0400

    Remove old flask

--- testflask/User.py
+++
@@ -1,9 +0,0 @@
-
-class User:
-   def __init__(self, username, password, public_key):
-     self.username = username
-	 self.password = password
-	 self.public_key = public_key
-	 
-   def __str__(self):
-     return "Username: " + self.username
